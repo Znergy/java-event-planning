a31514fba32276de22238791775bf1f95f92946e
Ryan <ryanjonesirl@gmail.com>
2017-03-17 16:47:35 -0700

    add another part of the front-end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,12 @@ public class App {
 
   public static void main(String[] args) {
     boolean isOver = false;
-    Console myConsole = System.console();
 
+    Console myConsole = System.console();
+    System.out.println("Welcome, lets plan a party!");
+    System.out.println("What would you like to plan first?");
     while(!isOver) {
-      System.out.println("Welcome, lets plan a party!");
-      System.out.println("What would you like to plan first? Type 'guests', 'food', 'drinks', or 'entertainment'. If at any point you want to quit, type 'quit'.");
+      System.out.println("Type 'guests', 'food', 'drinks', 'entertainment', or 'quit'.");
       String choice = myConsole.readLine();
       if(choice.equals("quit")) {
         System.out.println("Thanks, have a good day!");
@@ -17,8 +18,20 @@ public class App {
         System.out.println("Alright, so how many guests are coming?");
         int guestAmount = Integer.parseInt(myConsole.readLine());
       } else if (choice.equals("food")) {
-        System.out.println("Alright, so how many guests are coming?");
+        System.out.println("What type's of food would you like to bring?");
+        System.out.println("Type 'add' to add a food, 'remove' to remove a food, and 'continue' to move on.");
+        String foodChoice = myConsole.readLine();
         
+        if(foodChoice.equals("add")) {
+
+        } else if(foodChoice.equals("remove")) {
+
+        } else if(foodChoice.equals("continue")) {
+
+        } else {
+
+        }
+
       }
     }
   }
