e15815540f300ca6a2be0e7c3cd22658daf551c2
Ryan <ryanjonesirl@gmail.com>
2017-03-17 16:37:54 -0700

    add part of the front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,25 @@
+import java.io.Console;
+
 public class App {
 
   public static void main(String[] args) {
-    
+    boolean isOver = false;
+    Console myConsole = System.console();
+
+    while(!isOver) {
+      System.out.println("Welcome, lets plan a party!");
+      System.out.println("What would you like to plan first? Type 'guests', 'food', 'drinks', or 'entertainment'. If at any point you want to quit, type 'quit'.");
+      String choice = myConsole.readLine();
+      if(choice.equals("quit")) {
+        System.out.println("Thanks, have a good day!");
+        isOver = true;
+      } else if (choice.equals("guests")) {
+        System.out.println("Alright, so how many guests are coming?");
+        int guestAmount = Integer.parseInt(myConsole.readLine());
+      } else if (choice.equals("food")) {
+        System.out.println("Alright, so how many guests are coming?");
+        
+      }
+    }
   }
 }
