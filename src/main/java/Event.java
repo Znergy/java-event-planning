e749c19b71936a4acff343c01f0cd8408377afab
Ryan <ryanjonesirl@gmail.com>
2017-03-17 08:47:29 -0700

    add the basic java setup and create the first test case. 1st test case, testing constructor, passed.

---
+++ src/main/java/Event.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ryanjones on 3/17/17.
+ */
+public class Event {
+
+  private int numberOfGuests;
+  private List<String> foodBeingServed;
+  private List<String> drinksBeingServed;
+  private String typeOfEntertainment;
+
+
+  public Event(int numberOfGuests, List<String> foodBeingServed, List<String> drinksBeingServed, String typeOfEntertainment) {
+      this.numberOfGuests = numberOfGuests;
+      this.foodBeingServed = foodBeingServed;
+      this.drinksBeingServed = drinksBeingServed;
+      this.typeOfEntertainment = typeOfEntertainment;
+  }
+
+  // public int getGuests() {
+  //   return this.numberOfGuests;
+  // }
+
+
+
+}
---
+++ src/test/java/EventTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class EventTest {
+
+  // First test (constuctor)
+  @Test
+  public void eventConstuctor_createInstance_true() {
+    List<String> food = new ArrayList<String>();
+    List<String> drink = new ArrayList<String>();
+    Event event = new Event(10, food, drink, "Band");
+    assertEquals(true, event instanceof Event);
+  }
+
+  // // Second test (getter for guests)
+  // @Test
+  // public void getGuests_returnsGuestNumber_10() {
+  //   Event event = new Event(10);
+  //   assertEquals(10, event.getGuests());
+  // }
+}
